fea7f806ab555d51a1572b770053f99116d74334
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-27 21:57:43 +0100

    files

--- src/test/java/AdderTest.java
+++ src/test/java/AdderTest.java
@@ -1,4 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AdderTest {
-  
+
+public class AdderTest {
+    @Test
+    public void testThatAdderClassCanUseTheFunctionFromAClassItExtends(){
+        Adder adder = new Adder();
+        int result = adder.add(1, 2);
+        assertEquals(3, result);
+        System.out.println(adder.getClass().getSuperclass().getName());
+    }
+
+    @Test
+    public void testThatadderSuperClassIsAritmethic(){
+        Adder adder = new Adder();
+        assertEquals("Arithmetic", adder.getClass().getSuperclass().getName());
+    }
+
+
 }
--- src/test/java/AnagramTest.java
+++ src/test/java/AnagramTest.java
@@ -1,4 +1,24 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-class AnagramTest {
-  
+
+public class AnagramTest {
+    @Test
+    public void testThatCatAndActAreAnagram(){
+        String first = "caT";
+        String second = "Act";
+        assertTrue(Anagram.isAnagram(first, second));
+    }
+    @Test
+    public void testThatDogAndRatAreNotAnagram(){
+        String first = "Dog";
+        String second = "Rat";
+        assertFalse(Anagram.isAnagram(first, second));
+    }
+    @Test
+    public void testThatCatAndActAreNotAnagram(){
+        String first = "anagramm";
+        String second = "marganaa";
+        assertFalse(Anagram.isAnagram(first, second));
+    }
+
 }
